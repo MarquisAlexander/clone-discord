e4546025f8c1acb85cc2c7927b688ca4906cb5fc
marquisalexander <marquissantos123@gmail.com>
2020-06-27 12:43:48 -0300

    finalizado a aplicação de clone do discord

--- src/components/ChannelData/index.tsx
+++ src/components/ChannelData/index.tsx
@@ -1,12 +1,83 @@
-import React from 'react';
+import React, {useRef, useEffect} from 'react';
+
+import ChannelMessage, {Mention} from '../ChannelMessage';
 
 import { Container, Messages, InputWrapper, Input, InputIcon } from './styles'
 
 
-const ServerName: React.FC = () => {
+const ChannelData: React.FC = () => {
+
+    const messagesRef = useRef() as React.MutableRefObject<HTMLDivElement>;
+
+    useEffect(() => {
+        const div = messagesRef.current;
+
+        if (div) {
+            div.scrollTop = div.scrollHeight;
+        }
+    }, [messagesRef]);
+
     return (
         <Container>
-            <Messages />
+            <Messages ref={messagesRef}>
+                <ChannelMessage
+                author="Marquis Alexander"
+                date="28/08/2020"
+                content="Hoje é meu aniversário!"
+                />
+                <ChannelMessage
+                author="Marquis Alexander"
+                date="28/08/2020"
+                content="Hoje é meu aniversário!"
+                />
+                <ChannelMessage
+                author="Marquis Alexander"
+                date="28/08/2020"
+                content="Hoje é meu aniversário!"
+                />
+                <ChannelMessage
+                author="Marquis Alexander"
+                date="28/08/2020"
+                content="Hoje é meu aniversário!"
+                />
+                <ChannelMessage
+                author="Marquis Alexander"
+                date="28/08/2020"
+                content="Hoje é meu aniversário!"
+                /><ChannelMessage
+                author="Marquis Alexander"
+                date="28/08/2020"
+                content="Hoje é meu aniversário!"
+                />
+                <ChannelMessage
+                author="Marquis Alexander"
+                date="28/08/2020"
+                content="Hoje é meu aniversário!"
+                />
+                <ChannelMessage
+                author="Marquis Alexander"
+                date="28/08/2020"
+                content="Hoje é meu aniversário!"
+                />
+                <ChannelMessage
+                author="Marquis Alexander"
+                date="28/08/2020"
+                content="Hoje é meu aniversário!"
+                />
+                <ChannelMessage 
+                author="Sagaz limitado"
+                date="28/08/2020"
+                content={
+                    <>
+                        <Mention>@Marquis Alexander</Mention>, me carrega no cops de novo
+                        por favor?
+                    </>
+                }
+                hasMention
+                isBot
+                />
+
+            </Messages>
 
             <InputWrapper>
                 <Input type="text" placeholder="Conversar em #chat-livre" />
@@ -16,4 +87,4 @@ const ServerName: React.FC = () => {
     );
 };
 
-export default ServerName;
+export default ChannelData;
